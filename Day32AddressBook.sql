b9443475374884378df4aa7c7e728188abe67922
mahesh <gangurdemahesh9@gmail.com>
2022-11-28 16:20:24 +0530

    [mahesh]add UC2 file

--- Day32AddressBook.sql
+++ Day32AddressBook.sql
@@ -1,1 +1,16 @@
-create database AddressBook_day32
+/*UC1*/
+create database AddressBook_day32 
+
+/*UC2*/
+create table addressbooktable
+(
+FirstName varchar(255),
+LastNames varchar(255), 
+Address varchar(255), 
+City varchar(255),
+State varchar(255),
+Zip int, 
+PhoneNumber varchar(15),
+Email varchar(100)
+)
+
